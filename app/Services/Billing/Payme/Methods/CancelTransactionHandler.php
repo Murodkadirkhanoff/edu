37637a990587c477e63a69cb1e167774717bb0f8
Murodkadirkhanoff <37834093+Murodkadirkhanoff@users.noreply.github.com>
2025-08-25 15:48:10 +0500

    bugs fixed

--- app/Services/Billing/Payme/Methods/CancelTransactionHandler.php
+++ app/Services/Billing/Payme/Methods/CancelTransactionHandler.php
@@ -2,7 +2,98 @@
 
 namespace App\Services\Billing\Payme\Methods;
 
-class CancelTransactionHandler
+use Goodoneuz\PayUz\Http\Classes\DataFormat;
+use Goodoneuz\PayUz\Http\Classes\Payme\Response;
+use Goodoneuz\PayUz\Models\Transaction;
+use Goodoneuz\PayUz\Services\PaymentService;
+
+class CancelTransactionHandler implements PaymeMethodHandler
 {
 
+    public function handle(array $params): array
+    {
+        $id = $params['id'];
+        $transaction = \App\Models\Transaction::where('provider_transaction_id', $id)->first();
+
+        if (!$transaction) {
+            return [
+                "error" => [
+                    "code" => -31003,
+                    "message" => [
+                        "ru" => "Transaction not found",
+                        "uz" => "Transaction not found",
+                        "en" => "Transaction not found"
+                    ],
+                    "data" => "timeout"
+                ]
+            ];
+        }
+
+        switch ($transaction->state) {
+            case -1:
+            case -2:
+                return [
+                    'result' => [
+                        'transaction' => (string)$transaction->id,
+                        'cancel_time' => 1 * $transaction->canceled_at,
+                        'state' => 1 * $transaction->state,
+                    ]
+                ];
+
+            case 1:
+                $transaction->cancel(1 * $transaction->reason);
+
+                $cancel_time =now()->valueOf();
+
+                $transaction->update([
+                    'canceled_at' => $cancel_time
+                ]);
+
+//                PaymentService::payListener(null, $transaction, 'cancel-pay');
+
+                return [
+                    'result' => [
+                        'transaction' => (string)$transaction->id,
+                        'cancel_time' => 1 * $transaction->canceled_at,
+                        'state' => 1 * $transaction->state,
+                    ]
+                ];
+
+                break;
+
+            case 2:
+                $transaction->cancel(1 * $transaction->reason);
+
+
+                $transaction->update([
+                    'canceled_at' => now()->valueOf(),
+                ]);
+
+//                PaymentService::payListener(null, $transaction, 'cancel-pay');
+
+                return [
+                    'result' => [
+                        'transaction' => (string)$transaction->id,
+                        'canceled_at' => 1 * $transaction->canceled_at,
+                        'state' => 1 * $transaction->state,
+                    ]
+                ];
+
+                break;
+
+            default:
+                return [
+                    "error" => [
+                        "code" => -31007,
+                        "message" => [
+                            "ru" => "Could not cancel transaction. Invalid state.",
+                            "uz" => "Could not cancel transaction. Invalid state.",
+                            "en" => "Could not cancel transaction. Invalid state."
+                        ],
+                        "data" => "timeout"
+                    ]
+                ];
+                break;
+        }
+    }
 }
