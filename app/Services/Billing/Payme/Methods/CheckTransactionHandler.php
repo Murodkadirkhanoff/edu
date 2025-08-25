137c1bdaad8d4ece458dc38405e80d90c94666c3
Murodkadirkhanoff <37834093+Murodkadirkhanoff@users.noreply.github.com>
2025-08-25 14:49:32 +0500

    bugs fixed

--- app/Services/Billing/Payme/Methods/CheckTransactionHandler.php
+++ app/Services/Billing/Payme/Methods/CheckTransactionHandler.php
@@ -2,7 +2,40 @@
 
 namespace App\Services\Billing\Payme\Methods;
 
-class CheckTransactionHandler
+use App\Models\Transaction;
+use Goodoneuz\PayUz\Http\Classes\Payme\Response;
+
+class CheckTransactionHandler implements PaymeMethodHandler
 {
 
+    public function handle(array $params): array
+    {
+        $id = $params['id'];
+
+        $transaction = Transaction::find($id);
+        if (!$transaction) {
+            return [
+                "error" => [
+                    "code" => -31003,
+                    "message" => [
+                        "ru" => "Transaction not found",
+                        "uz" => "Transaction not found",
+                        "en" => "Transaction not found"
+                    ],
+                    "data" => "timeout"
+                ]
+            ];
+        }
+
+        return [
+            'result' => [
+                'create_time' => 1 * $transaction->provider_created_at,
+                'perform_time' => 1,
+                'cancel_time' => 1,
+                'transaction' => (string)$transaction->id,
+                'state' => 1 * $transaction->provider_state,
+                'reason' => null,
+            ]
+        ];
+    }
 }
