e8c07c6293e047694f40cbfcc2b20caddef854af
Murodkadirkhanoff <37834093+Murodkadirkhanoff@users.noreply.github.com>
2025-08-25 15:22:58 +0500

    bugs fixed

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Goodoneuz\PayUz\Http\Classes\DataFormat;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
@@ -9,4 +10,27 @@ class Transaction extends Model
     protected $table = 'billings.transactions';
 
     public $guarded = [];
+
+    public function isExpired()
+    {
+        return ($this->provider_state == 1) && ($this->updated_at < now()->subHours(12));
+    }
+
+    public function cancel($reason)
+    {
+        $updated_time = now()->valueOf();
+
+        if ($this->state == 2) {
+            // Scenario: CreateTransaction -> PerformTransaction -> CancelTransaction
+            $this->state = -2;
+        } else {
+            // Scenario: CreateTransaction -> CancelTransaction
+            $this->state = -1;
+        }
+
+        $this->reason = $reason;
+        $this->canceled_at = $updated_time;
+
+        $this->update();
+    }
 }
--- app/Services/Billing/Payme/Methods/PerformTransactionHandler.php
+++ app/Services/Billing/Payme/Methods/PerformTransactionHandler.php
@@ -2,7 +2,88 @@
 
 namespace App\Services\Billing\Payme\Methods;
 
-class PerformTransactionHandler
+
+
+class PerformTransactionHandler implements PaymeMethodHandler
 {
 
+    public function handle(array $params): array
+    {
+        $id = $params['id'];
+        $transaction =  \App\Models\Transaction::where('provider_transaction_id', $id)->first();
+
+        // if transaction not found, send error
+        if (!$transaction) {
+            return [
+                "error" => [
+                    "code" => -31003,
+                    "message" => [
+                        "ru" => "Transaction not found",
+                        "uz" => "Transaction not found",
+                        "en" => "Transaction not found"
+                    ],
+                    "data" => "timeout"
+                ]
+            ];
+        }
+
+        switch ($transaction->state) {
+            case 1:
+                if ($transaction->isExpired()) {
+                    $transaction->cancel(4);
+
+
+                    return [
+                        "error" => [
+                            "code" => -31008,
+                            "message" => [
+                                "ru" => "Transaction is expired.",
+                                "uz" => "Transaction is expired.",
+                                "en" => "Transaction is expired."
+                            ],
+                            "data" => "timeout"
+                        ]
+                    ];
+                } else {
+
+                    $perform_time = now()->valueOf();
+                    $transaction->state = 2;
+                    $transaction->updated_at = $perform_time;
+                    $transaction->performed_at = $perform_time;
+                    $transaction->update();
+
+//                    PaymentService::payListener(null, $transaction, 'after-pay');
+
+                    return [
+                        'result' => [
+                            'transaction' => (string)$transaction->id,
+                            'perform_time' => $transaction->performed_at,
+                            'state' => (int) $transaction->provider_state,
+                        ]
+                    ];
+                }
+
+            case 2: // handle complete transaction
+                return [
+                    'result' => [
+                        'transaction' => (string) $transaction->id,
+                        'perform_time' => 1 * $transaction->performed_at,
+                        'state' => 1 * $transaction->state,
+                    ]
+                ];
+
+            default:
+                return [
+                    "error" => [
+                        "code" => -31008,
+                        "message" => [
+                            "ru" => "Could not perform this operation.",
+                            "uz" => "Could not perform this operation.",
+                            "en" => "Could not perform this operation."
+                        ],
+                        "data" => "timeout"
+                    ]
+                ];
+        }
+    }
 }
